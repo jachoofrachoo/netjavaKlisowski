80787bf65e022094cdbdf5947c234dc20fcb89ef
Jan Klisowski <janklisowski@MacBook-Air-Jan.local>
2024-03-03 20:07:49 +0100

    FizzBuzz

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace FizzBuzz;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj górny zakres liczb:");
+        int uB = Convert.ToInt32(Console.ReadLine());
+
+        FizzBuzz fizzBuzz = new FizzBuzz(uB);
+        fizzBuzz.Display();
+    }
+}
+
